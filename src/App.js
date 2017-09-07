5345fe72d8b7f410fe7d9bc332157e97d5b6d1a5
taeshik Hwang <ghjkghj@naver.com>
2017-09-07 15:27:15 +0900

    Create Basic Routes with rr4

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Home = () => <h1>Home</h1>;
+
+const App = () => (
+	<Router>
+		<div>
+			<Route exact path='/' component={Home} />
+			<Route path='/about' component={Home} />
+		</div>
+	</Router>
+);
+
+export default App;
